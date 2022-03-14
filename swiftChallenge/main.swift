97243fdc26633674954cd9489e4ff462fe89d421
Gabriel Santiago <gaaasts@gmail.com>
2022-03-14 11:53:41 -0300

    aaaaa

--- swiftChallenge/main.swift
+++ swiftChallenge/main.swift
@@ -7,10 +7,36 @@
 
 import Foundation
 
+
+print("\nSeja bem vindo! O xxxxxx funciona como uma calculadora de horas de sono, onde você pode acompanhar um panorama geral do tempo e da qualidade das horas dormidas. Ela funciona de maneira simples: para cada dia da semana, você deve inserir uma quantidade aproximada das horas que dormiu naquela noite e em seguida avaliar a qualidade do sono em uma nota dentro de uma escala de 0 a 5.  ")
+
+print("\n\nVamos começar?\n\n\n")
+
+
+//let qualitempo : (Int, Int) = (7, 5)
+//let Dicionario = ["Segunda": qualitempo ]
+
+
+func getSleepQuality(day: String) {
+
+    //Print dia
+    //pego tempo do sono
+    //pega o tempo da qualidade
+    //faz o cast
+    //salva no dict
+    
+    print(day)
+    
+    
+}
+    
+
+
 //segunda
-print("Em média, quantas horas você dormiu na noite de Segunda? ")
+print("SEGUNDA")
+print("Tempo de sono: ")
 let tempSegStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiSegStr = readLine()!
 
 //unwrapp e typecast segunda
@@ -19,9 +45,10 @@ guard let tempSegInt = Int(tempSegStr), let qualiSegInt = Int(qualiSegStr) else 
 }
 
 //terça
-print("Em média, quantas horas você dormiu na noite de terça? ")
+print("\nTERÇA")
+print("Temo de sono:")
 let tempTerStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiTerStr = readLine()!
 
 //unwrapp e typecast terça
@@ -30,9 +57,10 @@ guard let tempTerInt = Int(tempTerStr), let qualiTerInt = Int(qualiTerStr) else 
 }
 
 //quarta
-print("Em média, quantas horas você dormiu na noite de Quarta? ")
+print("\nQUARTA")
+print("Tempo de sono: ")
 let tempQuaStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiQuaStr = readLine()!
 
 //unwrapp e typecast quarta
@@ -41,9 +69,10 @@ guard let tempQuaInt = Int(tempQuaStr), let qualiQuaInt = Int(qualiQuaStr) else 
 }
 
 //quinta
-print("Em média, quantas horas você dormiu na noite de Quinta? ")
+print("\nQUINTA")
+print("Tempo de sono: ")
 let tempQuiStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiQuiStr = readLine()!
 
 //unwrapp e typecast quinta
@@ -52,9 +81,10 @@ guard let tempQuiInt = Int(tempQuiStr), let qualiQuiInt = Int(qualiQuiStr) else 
 }
 
 //sexta
-print("Em média, quantas horas você dormiu na noite de Sexta? ")
+print("\nSEXTA")
+print("Tempo de sono: ")
 let tempSexStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiSexStr = readLine()!
 
 //unwrapp e typecast Sexta
@@ -63,9 +93,10 @@ guard let tempSexInt = Int(tempSexStr), let qualiSexInt = Int(qualiSexStr) else 
 }
 
 //sábado
-print("Em média, quantas horas você dormiu na noite de Sábado? ")
+print("\nSÁBADO")
+print("Tempo de sono: ")
 let tempSabStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiSabStr = readLine()!
 
 //unwrapp e typecast sábado
@@ -74,9 +105,10 @@ guard let tempSabInt = Int(tempSabStr), let qualiSabInt = Int(qualiSabStr) else 
 }
 
 //domingo
-print("Em média, quantas horas você dormiu na noite de Domingo? ")
+print("\nDOMINGO")
+print("Tempo de sono: ")
 let tempDomStr = readLine()!
-print("Em uma escala de zero a cinco, como você avalia a qualidade desse sono?")
+print("Qualidade: ")
 let qualiDomStr = readLine()!
 
 //unwrapp e typecast domingo
@@ -93,8 +125,8 @@ let mediaTempSemana = totalHorasSemana / 7
 let mediaQualidadeSemana = (qualiSegInt + qualiTerInt + qualiQuaInt + qualiQuiInt + qualiSexInt + qualiSabInt + qualiDomInt) / 7
 
 //saidas
-print("\n\nAo longo dessa semana, você dormiu um total de \(totalHorasSemana) horas de sono, tendo uma média de sono de, aproximadamente, \(mediaTempSemana) por dia")
-print("em média, seu sono apresentou uma nota \(mediaQualidadeSemana)")
+print("\n\nAo longo dessa semana, você dormiu um total de \(totalHorasSemana) horas, tendo uma média de, aproximadamente, \(mediaTempSemana) horas dormidas por noite")
+print("Seu sono apresentou uma \(mediaQualidadeSemana) de qualidade")
 
     
 
