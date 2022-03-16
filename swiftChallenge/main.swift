c256f9ed1f1ad9de46262278ec05fea64e39796b
Gabriel Santiago <gaaasts@gmail.com>
2022-03-16 16:19:24 -0300

    main

--- swiftChallenge/main.swift
+++ swiftChallenge/main.swift
@@ -28,10 +28,24 @@ func getSleepInfo (day : String) {
     
     weekDict[day] = sleep
     
-    print("\n")
+    print("")
 }
 
-print("Welcome to SleepTracker! This program works as a sleeping hours calculator, in which you can track more closely the overall quality\nof your sleep weekly. It works in a very straightforward and simple way: for each day in the week, you must insert an approximate\namount of the hours you slept in the previous night, as well as a grade, in a scale from 0 to 5, of how well you consider you're\nfeeling after waking up.\n\nShall we begin?\n\n   ")
+
+print("""
+________________________________________________________________________________________________________________________
+|                                                   SLEEP TRACKER                                                      |
+|______________________________________________________________________________________________________________________|
+| Welcome to SleepTracker! This program works as a sleeping hours calculator, in which you can track more closely the  |
+| overall quality of your sleep weekly. It works in a very straightforward and simple way: for each day in the week,   |
+| you must insert an approximate amount of the hours you slept in the previous night, as well as a grade, in a scale   |
+| from 0 to 5, of how well you consider you're feeling after waking up.                                                |
+|                                                                                                                      |
+| Shall we begin?                                                                                                      |
+------------------------------------------------------------------------------------------------------------------------
+
+
+""")
 
 getSleepInfo(day: "MONDAY")
 getSleepInfo(day: "TUESDAY")
@@ -43,8 +57,61 @@ getSleepInfo(day: "SUNDAY")
 
 let totalHoursWeekly = weekDict["MONDAY"]!.hoursOfSleep + weekDict["TUESDAY"]!.hoursOfSleep + weekDict["WEDNESDAY"]!.hoursOfSleep + weekDict["THURSDAY"]!.hoursOfSleep + weekDict["FRIDAY"]!.hoursOfSleep + weekDict["SATURDAY"]!.hoursOfSleep + weekDict["SUNDAY"]!.hoursOfSleep
 
-let sleepQualityWeekly = weekDict["MONDAY"]!.sleepQuality + weekDict["TUESDAY"]!.sleepQuality + weekDict["WEDNESDAY"]!.sleepQuality + weekDict["THURSDAY"]!.sleepQuality + weekDict["FRIDAY"]!.sleepQuality + weekDict["SATURDAY"]!.sleepQuality + weekDict["SUNDAY"]!.sleepQuality
+let sleepQualityWeekly = (weekDict["MONDAY"]!.sleepQuality + weekDict["TUESDAY"]!.sleepQuality + weekDict["WEDNESDAY"]!.sleepQuality + weekDict["THURSDAY"]!.sleepQuality + weekDict["FRIDAY"]!.sleepQuality + weekDict["SATURDAY"]!.sleepQuality + weekDict["SUNDAY"]!.sleepQuality) / 7
 
-print("Hours slept this week : \(totalHoursWeekly)")
-print("Average hours per day : \(totalHoursWeekly / 7)")
+
+print("""
+
+ ***********************
+ * YOUR WEEKLY RESULTS *
+ ***********************
+
+""")
+print("- Hours slept this week : \(totalHoursWeekly)")
+print("- Average hours per night : \(totalHoursWeekly / 7)")
+if (totalHoursWeekly < 49) {
+    print("- You slept \(49 - totalHoursWeekly) hours less than the minumum recommended for an average adult")
+} else {
+    print("- You slept \(totalHoursWeekly - 49) hours more than the minimum recommended for an average adult.")
+}
+
+switch sleepQualityWeekly {
+    case 1:
+        print ("""
+
+- According to your evaluation, your sleep quality over this week can be considered very poor, and your sleep schedule must be urgently changed.
+  Medical literature strongly recommends that an adult should sleep at least 49 hours per week, and disrespecting this  minimum  amount  can
+  lead to serious health issues. You should consider checking the following materials:
+
+
+ _________________________________________________________________________________________
+ |                  The Effects of Sleep Deprivation on Your Body:                       |
+ |_______________________________________________________________________________________|
+ | https://www.healthline.com/health/sleep-deprivation/effects-on-body#Respiratory-system|
+ -----------------------------------------------------------------------------------------
+ _________________________________________________________________________________________
+ |                  Why Can't I Sleep? Reasons for Trouble Sleeping:                     |
+ |_______________________________________________________________________________________|
+ |   https://www.verywellmind.com/reasons-for-not-sleeping-well-and-how-to-fix-350760    |
+ -----------------------------------------------------------------------------------------
+ _________________________________________________________________________________________
+ |              Poor Sleep Hygiene: Don't Let It Ruin Your Quality of Life               |
+ |_______________________________________________________________________________________|
+ |                 https://www.risescience.com/blog/poor-sleep-hygiene                   |
+ -----------------------------------------------------------------------------------------
+
+
+""")
+
+    case 2:
+        print("")
+    case 3:
+        print("")
+    case 4:
+        print("")
+    case 5:
+        print("")
+    default:
+        break
+}
 
