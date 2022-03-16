f2a5d7c0b4a8e32f91105e0f65d44aae29267c3b
Gabriel Santiago <gaaasts@gmail.com>
2022-03-16 11:50:57 -0300

    aaaa

--- swiftChallenge/main.swift
+++ swiftChallenge/main.swift
@@ -7,126 +7,44 @@
 
 import Foundation
 
+var weekDict : [String : (hoursOfSleep : Int, sleepQuality : Int)] = [:]
 
-print("\nSeja bem vindo! O xxxxxx funciona como uma calculadora de horas de sono, onde você pode acompanhar um panorama geral do tempo e da qualidade das horas dormidas. Ela funciona de maneira simples: para cada dia da semana, você deve inserir uma quantidade aproximada das horas que dormiu naquela noite e em seguida avaliar a qualidade do sono em uma nota dentro de uma escala de 0 a 5.  ")
-
-print("\n\nVamos começar?\n\n\n")
-
-
-//let qualitempo : (Int, Int) = (7, 5)
-//let Dicionario = ["Segunda": qualitempo ]
-
-
-func getSleepQuality(day: String) {
-
-    //Print dia
-    //pego tempo do sono
-    //pega o tempo da qualidade
-    //faz o cast
-    //salva no dict
+func getSleepInfo (day : String) {
+    
+    var sleep : (Int, Int)
     
     print(day)
+    print("Hours of sleep: ")
+    let hours = readLine()!
+    print("Quality score: ")
+    let quality = readLine()!
     
+    guard var hoursOfSleep = Int(hours), var sleepQuality = Int(quality) else {
+        exit(0)
+    }
     
-}
+    sleep.0 = hoursOfSleep
+    sleep.1 = sleepQuality
     
-
-
-//segunda
-print("SEGUNDA")
-print("Tempo de sono: ")
-let tempSegStr = readLine()!
-print("Qualidade: ")
-let qualiSegStr = readLine()!
-
-//unwrapp e typecast segunda
-guard let tempSegInt = Int(tempSegStr), let qualiSegInt = Int(qualiSegStr) else {
-    exit(0)
-}
-
-//terça
-print("\nTERÇA")
-print("Temo de sono:")
-let tempTerStr = readLine()!
-print("Qualidade: ")
-let qualiTerStr = readLine()!
-
-//unwrapp e typecast terça
-guard let tempTerInt = Int(tempTerStr), let qualiTerInt = Int(qualiTerStr) else {
-    exit(0)
-}
-
-//quarta
-print("\nQUARTA")
-print("Tempo de sono: ")
-let tempQuaStr = readLine()!
-print("Qualidade: ")
-let qualiQuaStr = readLine()!
-
-//unwrapp e typecast quarta
-guard let tempQuaInt = Int(tempQuaStr), let qualiQuaInt = Int(qualiQuaStr) else {
-    exit(0)
-}
-
-//quinta
-print("\nQUINTA")
-print("Tempo de sono: ")
-let tempQuiStr = readLine()!
-print("Qualidade: ")
-let qualiQuiStr = readLine()!
-
-//unwrapp e typecast quinta
-guard let tempQuiInt = Int(tempQuiStr), let qualiQuiInt = Int(qualiQuiStr) else {
-    exit(0)
-}
-
-//sexta
-print("\nSEXTA")
-print("Tempo de sono: ")
-let tempSexStr = readLine()!
-print("Qualidade: ")
-let qualiSexStr = readLine()!
-
-//unwrapp e typecast Sexta
-guard let tempSexInt = Int(tempSexStr), let qualiSexInt = Int(qualiSexStr) else {
-    exit(0)
-}
-
-//sábado
-print("\nSÁBADO")
-print("Tempo de sono: ")
-let tempSabStr = readLine()!
-print("Qualidade: ")
-let qualiSabStr = readLine()!
-
-//unwrapp e typecast sábado
-guard let tempSabInt = Int(tempSabStr), let qualiSabInt = Int(qualiSabStr) else {
-    exit(0)
-}
-
-//domingo
-print("\nDOMINGO")
-print("Tempo de sono: ")
-let tempDomStr = readLine()!
-print("Qualidade: ")
-let qualiDomStr = readLine()!
-
-//unwrapp e typecast domingo
-guard let tempDomInt = Int(tempDomStr), let qualiDomInt = Int(qualiDomStr) else {
-    exit(0)
-}
-
-
-
-
-// médias
-let totalHorasSemana = (tempSegInt + tempTerInt + tempQuaInt + tempQuiInt + tempSegInt + tempSabInt + tempDomInt)
-let mediaTempSemana = totalHorasSemana / 7
-let mediaQualidadeSemana = (qualiSegInt + qualiTerInt + qualiQuaInt + qualiQuiInt + qualiSexInt + qualiSabInt + qualiDomInt) / 7
-
-//saidas
-print("\n\nAo longo dessa semana, você dormiu um total de \(totalHorasSemana) horas, tendo uma média de, aproximadamente, \(mediaTempSemana) horas dormidas por noite")
-print("Seu sono apresentou uma \(mediaQualidadeSemana) de qualidade")
-
+    weekDict[day] = sleep
     
+    print("\n")
+}
+
+print("Welcome to SleepTracker! This program works as a sleeping hours calculator, in which you can track more closely the overall quality\nof your sleep weekly. It works in a very straightforward and simple way: for each day in the week, you must insert an approximate\namount of the hours you slept in the previous night, as well as a grade, in a scale from 0 to 5, of how well you consider you're\nfeeling after waking up.\n\nShall we begin?\n\n   ")
+
+getSleepInfo(day: "MONDAY")
+getSleepInfo(day: "TUESDAY")
+getSleepInfo(day: "WEDNESDAY")
+getSleepInfo(day: "THURSDAY")
+getSleepInfo(day: "FRIDAY")
+getSleepInfo(day: "SATURDAY")
+getSleepInfo(day: "SUNDAY")
+
+let totalHoursWeekly = weekDict["MONDAY"]!.hoursOfSleep + weekDict["TUESDAY"]!.hoursOfSleep + weekDict["WEDNESDAY"]!.hoursOfSleep + weekDict["THURSDAY"]!.hoursOfSleep + weekDict["FRIDAY"]!.hoursOfSleep + weekDict["SATURDAY"]!.hoursOfSleep + weekDict["SUNDAY"]!.hoursOfSleep
+
+let sleepQualityWeekly = weekDict["MONDAY"]!.sleepQuality + weekDict["TUESDAY"]!.sleepQuality + weekDict["WEDNESDAY"]!.sleepQuality + weekDict["THURSDAY"]!.sleepQuality + weekDict["FRIDAY"]!.sleepQuality + weekDict["SATURDAY"]!.sleepQuality + weekDict["SUNDAY"]!.sleepQuality
+
+print("Hours slept this week : \(totalHoursWeekly)")
+print("Average hours per day : \(totalHoursWeekly / 7)")
 
